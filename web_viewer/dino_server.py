9c08e3284972aacef4d64c6355d0fd45516d050c
diego0020 <gus0020@gmail.com>
2015-06-09 14:14:27 -0500

    questions in notebook, prototype of filter viewer

--- web_viewer/dino_server.py
+++ web_viewer/dino_server.py
@@ -8,33 +8,64 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("dino_map.html")
 
+class DinoFilter(tornado.web.RequestHandler):
+    def get(self):
+        self.render("dino_filter.html")
+
+class FilterData(tornado.web.RequestHandler):
+    def get(self):
+        x_min = float(self.get_argument("x_min"))
+        x_max = float(self.get_argument("x_max"))
+        y_min = float(self.get_argument("y_min"))
+        y_max = float(self.get_argument("y_max"))
+        t_min = np.datetime64(int(self.get_argument("t_min")),"ms")
+        t_max = np.datetime64(int(self.get_argument("t_max")),"ms")
+        df = self.df
+        area = (df["X"]<=x_max) & (df["X"]>=x_min) & (df["Y"]<=y_max) & (df["Y"]>=y_min)
+        time = (df["time"] >= t_min) & (df["time"] <= t_max)
+        guests = sorted([int(i) for i in np.unique(df.loc[area&time]["id"])])
+        self.write({"guests" : guests})
+
+    def initialize(self, df):
+        self.df = df
+
+
 class DataHandler(tornado.web.RequestHandler):
     def get(self):
         df = self.df
-        guest_id = np.random.choice(df["id"])
+        guest_id = self.get_argument("id", None)
+        if guest_id is None:
+            guest_id = np.random.choice(df["id"])
+        else:
+            guest_id = int(guest_id)
         guest_df = df.loc[df["id"]==guest_id]
         guest_df_list = guest_df.to_dict("records")        
         self.write({"array" :guest_df_list})
 
-    def initialize(self):
-        self.df = pd.read_csv("../../MC1 2015 Data/park-movement-Fri.csv")
-
-
+    def initialize(self, df):
+        self.df = df
 
 
 settings = {"template_path" : os.path.dirname(__file__),
-            "static_path" : os.path.dirname(__file__)+"static",
-            "autoreload": True,
+            "static_path" : os.path.join(os.path.dirname(__file__),"static"),
             } 
 
 if __name__ == "__main__":
+    path = os.path.join(os.path.dirname(__file__), "../../MC1 2015 Data/park-movement-Fri.csv")
+    df = pd.read_csv(path)
+    df2 = df.copy()
+    df2["time"] = pd.to_datetime(df2.Timestamp, format="%Y-%m-%d %H:%M:%S")
+
     application = tornado.web.Application([
         (r"/", MainHandler),
-        (r"/data", DataHandler),
+        (r"/data", DataHandler,{"df":df}),
+        (r"/filter", DinoFilter),
+        (r"/filter_data", FilterData,{"df":df2}),
         (r"/static/(.*)", tornado.web.StaticFileHandler,
             {"path": settings["static_path"]})
 
     ], **settings)
     application.listen(8100)
+    print("ready")
     tornado.ioloop.IOLoop.current().start()
 
